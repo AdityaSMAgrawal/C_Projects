8bd147880350de0796370de491330bf99c86f702
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-01-13 09:38:53 +0530

    program to print the words of a sentence seperately on new line

---
+++ sentence_slicer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int i,j,x=0;
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    for(i=0;i<strlen(s)+1;i++)
+    {
+        if(s[i]==' '||s[i]=='\0')
+        {
+            for(j=x;j<i;j++)
+            {
+                printf("%c",s[j]);
+            }
+            x = i + 1;
+            printf("\n");
+        }
+        else {
+        continue;
+        }
+    }
+    //Write your logic to print the tokens of the sentence here.
+    return 0;
+}
